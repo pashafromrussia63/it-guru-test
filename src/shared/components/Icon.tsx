593d0fcb3cf05a365aaf1993adafb4d473e14cb7
PashaFromRussia <pashafromrussia63@gmail.com>
2026-03-17 00:24:37 +0300

    update icon import

--- src/shared/components/Icon.tsx
+++ src/shared/components/Icon.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { ReactComponent as UserIcon } from '../../assets/icons/user.svg?react';
-import { ReactComponent as LockIcon } from '../../assets/icons/lock.svg?react';
-import { ReactComponent as CrossIcon } from '../../assets/icons/cross.svg?react';
-import { ReactComponent as EyeIcon } from '../../assets/icons/eye.svg?react';
-import { ReactComponent as EyeClosedIcon } from '../../assets/icons/eye_closed.svg?react';
-import { ReactComponent as RefreshIcon } from '../../assets/icons/refresh.svg?react';
+import UserIcon from '../../assets/icons/user.svg?react';
+import LockIcon from '../../assets/icons/lock.svg?react';
+import CrossIcon from '../../assets/icons/cross.svg?react';
+import EyeIcon from '../../assets/icons/eye.svg?react';
+import EyeClosedIcon from '../../assets/icons/eye_closed.svg?react';
+import RefreshIcon from '../../assets/icons/refresh.svg?react';
 
 const icons = {
     user: UserIcon,
